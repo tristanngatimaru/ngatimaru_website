742ac5184da5350fdd0dcc97f9cb023cb9253286
tristanngatimaru <tristan@ngatimaru.iwi.nz>
2025-07-15 13:43:49 +1200

    home page design edits and animation additions

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,49 +1,199 @@
+import { useState, useEffect } from "react";
+
 function Navbar() {
+  //line in nav bar should appear on refresh
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isAppeared, setIsAppeared] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadedTimeout = setTimeout(() => setIsLoaded(true), 500);
+    const expandedTimeout = setTimeout(() => setIsExpanded(true), 1000);
+    const appearedTimeout = setTimeout(() => setIsAppeared(true), 1500);
+
+    return () => {
+      clearTimeout(loadedTimeout);
+      clearTimeout(expandedTimeout);
+      clearTimeout(appearedTimeout);
+    };
+  }, []);
+
+  //text on nav bar appear out of the line
+
   return (
-    <div className="w-full items-center flex flex-col">
+    <div
+      className={`w-full items-center flex flex-col transition-all ease-in-out duration-500
+    ${isLoaded ? "opacity-100" : "opacity-0"}`}
+    >
       <div className="w-[1000px] flex flex-col items-center">
-        <span className="w-full h-0.5 rounded-3xl bg-gray-300 relative translate-y-[1.5rem] pointer-events-none"></span>
+        <span
+          className={`h-0.5 rounded-3xl bg-gray-300 relative translate-y-[1.55rem] pointer-events-none
+    transition-all duration-[1500ms] ease-in-out
+    ${isExpanded ? "w-full" : "w-1"}
+    
+  `}
+        />
+
         <nav className="w-full flex items-center justify-between ">
-          <a href="">
-            <div className="font-roboto-light items-center flex flex-col text-gray-300">
-              <h3 className="text-base">KAINGA</h3>
-              <h3 className="text-sm">HOME</h3>
+          <a href="" className="font-roboto-light text-gray-300">
+            <div className="items-center flex flex-col">
+              {/* Top text container */}
+              <div className="overflow-hidden h-6 w-full flex justify-center">
+                <h3
+                  className={`delay-500 text-base pb-0.5 transition-transform duration-700 ease-in-out
+          ${isAppeared ? "translate-y-0" : "translate-y-6"}`}
+                >
+                  KAINGA
+                </h3>
+              </div>
+
+              {/* Bottom text container */}
+              <div className="overflow-hidden h-4 w-full flex justify-center">
+                <h3
+                  className={` delay-500 text-xs pt-1 transition-transform duration-700 ease-in-out
+          ${isAppeared ? "translate-y-0" : "-translate-y-6"}`}
+                >
+                  HOME
+                </h3>
+              </div>
             </div>
           </a>
           <a href="" className="font-roboto-light text-gray-300">
             <div className="items-center flex flex-col">
-              <h3 className="text-base">KAINGA</h3>
-              <h3 className="text-sm">HOME</h3>
+              {/* Top text container */}
+              <div className="overflow-hidden h-6 w-full flex justify-center">
+                <h3
+                  className={`delay-300 text-base pb-0.5 transition-transform duration-700 ease-in-out
+          ${isAppeared ? "translate-y-0" : "translate-y-6"}`}
+                >
+                  KO WAI
+                </h3>
+              </div>
+
+              {/* Bottom text container */}
+              <div className="overflow-hidden h-4 w-full flex justify-center">
+                <h3
+                  className={`delay-300 text-xs pt-1 transition-transform duration-700 ease-in-out
+          ${isAppeared ? "translate-y-0" : "-translate-y-6"}`}
+                >
+                  ABOUT US
+                </h3>
+              </div>
             </div>
           </a>
           <a href="" className="font-roboto-light text-gray-300">
             <div className="items-center flex flex-col">
-              <h3 className="text-base">KAINGA</h3>
-              <h3 className="text-sm">HOME</h3>
+              {/* Top text container */}
+              <div className="overflow-hidden h-6 w-full flex justify-center">
+                <h3
+                  className={`text-base pb-0.5 transition-transform duration-700 ease-in-out
+          ${isAppeared ? "translate-y-0" : "translate-y-6"}`}
+                >
+                  RĀHITA MATAI WHETŪ
+                </h3>
+              </div>
+
+              {/* Bottom text container */}
+              <div className="overflow-hidden h-4 w-full flex justify-center">
+                <h3
+                  className={`text-xs pt-1 transition-transform duration-700 ease-in-out
+          ${isAppeared ? "translate-y-0" : "-translate-y-6"}`}
+                >
+                  BOOKING MATAI WHETŪ
+                </h3>
+              </div>
             </div>
           </a>
           <a href="" className="font-roboto-light text-gray-300">
             <div className="items-center flex flex-col">
-              <h3 className="text-base">KAINGA</h3>
-              <h3 className="text-sm">HOME</h3>
+              {/* Top text container */}
+              <div className="overflow-hidden h-6 w-full flex justify-center">
+                <h3
+                  className={`text-base pb-0.5 transition-transform duration-700 ease-in-out
+          ${isAppeared ? "translate-y-0" : "translate-y-6"}`}
+                >
+                  RIHITI HĪ IKA
+                </h3>
+              </div>
+
+              {/* Bottom text container */}
+              <div className="overflow-hidden h-4 w-full flex justify-center">
+                <h3
+                  className={`text-xs pt-1 transition-transform duration-700 ease-in-out
+          ${isAppeared ? "translate-y-0" : "-translate-y-6"}`}
+                >
+                  FISHING PERMIT
+                </h3>
+              </div>
             </div>
           </a>
           <a href="" className="font-roboto-light text-gray-300">
             <div className="items-center flex flex-col">
-              <h3 className="text-base">KAINGA</h3>
-              <h3 className="text-sm">HOME</h3>
+              {/* Top text container */}
+              <div className="overflow-hidden h-6 w-full flex justify-center">
+                <h3
+                  className={`delay-300 text-base pb-0.5 transition-transform duration-700 ease-in-out
+          ${isAppeared ? "translate-y-0" : "translate-y-6"}`}
+                >
+                  NGĀ TUHINGA
+                </h3>
+              </div>
+
+              {/* Bottom text container */}
+              <div className=" overflow-hidden h-4 w-full flex justify-center">
+                <h3
+                  className={`delay-300 text-xs pt-1 transition-transform duration-700 ease-in-out
+          ${isAppeared ? "translate-y-0" : "-translate-y-6"}`}
+                >
+                  DOCUMENTS
+                </h3>
+              </div>
+            </div>
+          </a>
+          <a href="" className="font-roboto-light text-gray-300 hidden">
+            <div className="items-center flex flex-col">
+              {/* Top text container */}
+              <div className="overflow-hidden h-6 w-full flex justify-center">
+                <h3
+                  className={`text-base pb-0.5 transition-transform duration-700 ease-in-out
+          ${isAppeared ? "translate-y-0" : "translate-y-6"}`}
+                >
+                  TOA
+                </h3>
+              </div>
+
+              {/* Bottom text container */}
+              <div className="overflow-hidden h-4 w-full flex justify-center">
+                <h3
+                  className={`text-xs pt-1 transition-transform duration-700 ease-in-out
+          ${isAppeared ? "translate-y-0" : "-translate-y-6"}`}
+                >
+                  STORE
+                </h3>
+              </div>
             </div>
           </a>
           <a href="" className="font-roboto-light text-gray-300">
             <div className="items-center flex flex-col">
-              <h3 className="text-base">KAINGA</h3>
-              <h3 className="text-sm">HOME</h3>
-            </div>
-          </a>
-          <a href="" className="font-roboto-light text-gray-300">
-            <div className="items-center flex flex-col">
-              <h3 className="text-base">KAINGA</h3>
-              <h3 className="text-sm">HOME</h3>
+              {/* Top text container */}
+              <div className="overflow-hidden h-6 w-full flex justify-center">
+                <h3
+                  className={`delay-500 text-base pb-0.5 transition-transform duration-700 ease-in-out
+          ${isAppeared ? "translate-y-0" : "translate-y-6"}`}
+                >
+                  RĒHITA
+                </h3>
+              </div>
+
+              {/* Bottom text container */}
+              <div className="overflow-hidden h-4 w-full flex justify-center">
+                <h3
+                  className={` delay-500 text-xs pt-1 transition-transform duration-700 ease-in-out
+          ${isAppeared ? "translate-y-0" : "-translate-y-6"}`}
+                >
+                  REGISTER
+                </h3>
+              </div>
             </div>
           </a>
         </nav>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,9 +3,25 @@ import axios from "axios";
 import Navbar from "../components/navbar";
 import veryCloseCarving from "../assets/images/headerimages/veryclosecarving.png";
 import korowai from "../assets/images/headerimages/korowai.png";
+import arrow from "../assets/images/icons/arrow.png";
 
 function Home() {
   const [posts, setPosts] = useState([]);
+  const [isStartUp, setStartUp] = useState(false);
+  const [isAppear, setAppear] = useState(false);
+  const [isButton, setButton] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => setStartUp(true), 500);
+    const appearTimeout = setTimeout(() => setAppear(true), 2000);
+    const buttonTimeout = setTimeout(() => setButton(true), 2000);
+
+    return () => {
+      clearTimeout(timeoutId);
+      clearTimeout(appearTimeout);
+      clearTimeout(buttonTimeout);
+    };
+  }, []);
 
   useEffect(() => {
     axios
@@ -15,12 +31,16 @@ function Home() {
   }, []);
 
   return (
-    <div className="w-screen">
-      <div className="relative w-screen h-full overflow-hidden">
+    <div className="w-screen overflow-hidden">
+      <div
+        className={`relative w-screen h-full overflow-hidden transition-all ease-in-out duration-500 ${
+          isStartUp ? "opacity-100" : "opacity-0"
+        } `}
+      >
         <img
           src={veryCloseCarving}
           alt=""
-          className="w-full h-screen object-center object-cover "
+          className="w-full h-[800px] object-center object-cover "
         />
 
         {/* Navbar on top of image */}
@@ -29,9 +49,56 @@ function Home() {
         </div>
 
         {/* Centered title text */}
-        <h1 className="absolute w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-7xl font-roboto-thin text-white text-center">
-          NGĀTI MARU RŪNANGA
-        </h1>
+        <div className="absolute w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center pt-28">
+          <div className="overflow-hidden h-[80px] w-full flex justify-center">
+            <h1
+              className={`transition-all duration-1000 ease-in-out text-7xl font-roboto-thin text-white text-center ${
+                isAppear ? "translate-y-0" : "translate-y-20"
+              }`}
+            >
+              NGĀTI MARU RŪNANGA
+            </h1>
+          </div>
+
+          {/* Space between title and buttons */}
+
+          <div className="mt-16 flex flex-col items-center gap-6">
+            {/* REGISTER Button */}
+            <div className="overflow-hidden h-[70px] w-full flex justify-center">
+              <button
+                className={`
+    text-center outline-2 outline-white rounded-full text-white font-bold w-[200px] h-[60px] m-2 font-roboto-thin text-2xl hover:gradient
+    transform
+    ${isAppear ? "translate-y-0" : "translate-y-20"}
+    ${
+      isAppear
+        ? "transition-transform duration-1000 ease-in-out"
+        : "transition-transform duration-200 ease-in-out"
+    }
+    hover:scale-105 hover:transition-transform hover:duration-200
+  `}
+              >
+                REGISTER
+              </button>
+            </div>
+
+            {/* ARROW Button */}
+            <div className="overflow-hidden h-[90px] w-full flex justify-center">
+              <button
+                className={` rounded-full
+        w-[60px] h-[60px] m-2 flex items-center justify-center
+        transform
+        ${isAppear ? "translate-y-0" : "translate-y-20"}
+        transition-transform duration-1000 ease-in-out
+        hover:scale-110 hover:transition-transform hover:duration-200
+        outline-2 outline-transparent hover:outline-white
+      `}
+              >
+                <img src={arrow} alt="" className="w-10" />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="h-[500px] flex flex-col overflow-hidden">
