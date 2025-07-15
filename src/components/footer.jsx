a760e357ddbef8d15ad2cc550a40aaa4443a648d
tristanngatimaru <tristan@ngatimaru.iwi.nz>
2025-07-16 09:52:40 +1200

    footer addition

---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  //text on nav bar appear out of the line
+
+  return (
+    <div className="flex w-full justify-between px-20 items-start">
+      <phone>PHONE</phone>
+      <office>OFFICE</office>
+      <postal>POSTAL</postal>
+      <email>EMAIL</email>
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import Navbar from "../components/navbar";
 import veryCloseCarving from "../assets/images/headerimages/veryclosecarving.png";
@@ -6,10 +6,16 @@ import korowai from "../assets/images/headerimages/korowai.png";
 import arrow from "../assets/images/icons/arrow.png";
 import arrowBlack from "../assets/images/icons/arrow_Black.png";
 import Posts from "../components/posts";
+import Footer from "../components/footer";
 
 function Home() {
   const [isStartUp, setStartUp] = useState(false);
   const [isAppear, setAppear] = useState(false);
+  const targetRef = useRef(null);
+
+  const handleScroll = () => {
+    targetRef.current.scrollIntoView({ behavior: "smooth" });
+  };
 
   useEffect(() => {
     const timeoutId = setTimeout(() => setStartUp(true), 500);
@@ -55,63 +61,59 @@ function Home() {
 
           <div className="mt-16 flex flex-col items-center gap-6">
             {/* REGISTER Button */}
-            <div className="overflow-hidden h-[70px] w-full flex justify-center">
-              <button
+            <div className="overflow-hidden h-[80px] w-full flex justify-center">
+              <div
                 className={`
-    text-center outline-2 outline-white rounded-full text-white font-bold w-[200px] h-[60px] m-2 font-roboto-thin text-2xl hover:gradient
-    transform
-    ${isAppear ? "translate-y-0" : "translate-y-20"}
-    ${
-      isAppear
-        ? "transition-transform duration-1000 ease-in-out"
-        : "transition-transform duration-200 ease-in-out"
-    }
-    hover:scale-105 hover:transition-transform hover:duration-200
-  `}
+      transition-all duration-1000 ease-in-out
+      ${isAppear ? "translate-y-0" : "translate-y-20"}
+    `}
               >
-                REGISTER
-              </button>
+                <button className="text-center outline-2 outline-white rounded-full text-white font-bold w-[200px] h-[60px] m-2 font-roboto-thin text-2xl hover:scale-105 duration-200 ease-in-out">
+                  REGISTER
+                </button>
+              </div>
             </div>
 
             {/* ARROW Button */}
             <div className="overflow-hidden h-[90px] w-full flex justify-center">
-              <button
+              <div
                 className={` rounded-full
         w-[60px] h-[60px] m-2 flex items-center justify-center
         transform
         ${isAppear ? "translate-y-0" : "translate-y-20"}
         transition-transform duration-1000 ease-in-out
-        hover:scale-110 hover:transition-transform hover:duration-200
-        outline-2 outline-transparent hover:outline-white
+        
       `}
               >
-                <img src={arrow} alt="" className="w-10" />
-              </button>
+                <button
+                  onClick={handleScroll}
+                  className="hover:scale-110 ease-in-out duration-200 hover:outline-white outline-transparent outline-2 rounded-full p-3"
+                >
+                  <img src={arrow} alt="" className="w-10" />
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="h-[500px] flex flex-row overflow-hidden">
+      <div ref={targetRef} className="h-[500px] flex flex-row overflow-hidden">
         <img src={korowai} alt="" className="object-cover w-1/2 h-full" />
         <div className="w-1/2 h-full bg-white flex flex-col items-center justify-center py-6 px-18 text-center">
-          <p className="font-roboto-light text-3xl">Nau mai, haere mai</p>
-          <p className="font-roboto-light text-3xl mt-10">
+          <p className="font-roboto-light text-3xl pb-10">Nau mai, haere mai</p>
+          <p className="font-roboto-light text-3xl ">
             Mai i Ngā Kuri-a-Whārei ki Tīkapa Moana, ko Hauraki te rohe whenua,
             ko Marutūāhu te tangata, ko Ngāti Maru te iwi. Tātou te hunga ora –
             hui e, tāiki e!
           </p>
-          <p className="font-roboto-light text-3xl mt-15 ">Full Mihi</p>
-          <button className="hover:outline-black outline-transparent outline-2 rounded-full p-10 ease-in-out duration-200">
-            <img
-              src={arrowBlack}
-              alt=""
-              className="w-10 hover:scale-110 ease-in-out duration-200"
-            />
+          <p className="font-roboto-light text-3xl pt-10">Full Mihi</p>
+          <button className="hover:outline-black hover:scale-110 ease-in-out duration-200  outline-transparent outline-2 rounded-full p-3">
+            <img src={arrowBlack} alt="" className="w-10 " />
           </button>
         </div>
       </div>
-      d
+
       <Posts />
+      <Footer />
     </div>
   );
 }
