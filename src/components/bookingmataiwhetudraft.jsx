8022d763274ad3689c9a03050fed0d2e66607406
tristanngatimaru <tristan@ngatimaru.iwi.nz>
2025-08-15 15:19:43 +1200

    hope

--- src/components/bookingmataiwhetudraft.jsx
+++ src/components/bookingmataiwhetudraft.jsx
@@ -2,7 +2,7 @@ import Footer from "../components/footer";
 import HeroHeader from "../components/header";
 import content from "../components/sitecontent/content";
 import { Images } from "../components/sitecontent/images";
-import FormComponent from "../components/formcomponent";
+import FormComponent from "../components/FormComponent";
 import { useState } from "react";
 
 function MataiWhetu() {
